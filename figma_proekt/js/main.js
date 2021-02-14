2488b46ad8f71c03747a2c943057f7120242f4f9
Episod86 <episod_kufar@mail.ru>
2021-02-14 15:26:23 +0300

    видео

--- figma_proekt/js/main.js
+++ figma_proekt/js/main.js
@@ -1,10 +1,13 @@
-// "use strict"
+'use strict'
 
 (function () {
     const header = document.querySelector('.menu');
     window.onscroll = () => {
         if (window.pageYOffset > 50) {
             header.classList.add('.js_menu');
+        }
+        else if (header.classList.contains("js_menu")) {
+            header.classList.remove("js_menu");
         }
     };
 } () );
