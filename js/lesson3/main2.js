dc8262466f2d0f615778e1e4eb8ff52beb7e548e
episod86 <episod_kufar@mail.ru>
2021-02-15 19:48:55 +0300

    домашка

---
+++ js/lesson3/main2.js
@@ -0,0 +1,8 @@
+'use strict'
+
+let a = 17, b= 10, c, d = 7, result;
+// c = a - b;
+// result = c + d;
+
+result = d + (c = a - b);
+alert(result);
---
+++ js/lesson3/main3.js
@@ -0,0 +1,6 @@
+'use strict'
+
+// let data = new Date();
+
+let hour = new Date().getHours(), minute = new Date().getMinutes(), second = new Date().getSeconds();
+alert(`${hour}:${minute}:${second}`);
