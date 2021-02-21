32556fe2d05cee4944ae5c1369543f6cd8c0cd1f
Episod86 <episod_kufar@mail.ru>
2021-02-21 10:59:00 +0300

    js-less3

--- js/lesson3/main4.js
+++ js/lesson3/main4.js
@@ -1,23 +1,45 @@
 'use strict';
 
-let maxWidth = 1024, margin = 5, kol = 4, width;
-width = (maxWidth - margin * 2 * kol) / kol; 
+const maxWidth = 1024, margin = 5; 
+let num = 4, width;
+width = (maxWidth - margin * 2 * num) / num; 
 alert(`Ширина изображения ${width} px`);
 
-let kolMax, kolImageRows, kolRows, kolOst;
-kolMax = +prompt('Введите количество изображений', kolMax);
-width = +prompt('Введите ширину изображения', width);
+let numMax, numImageRows, numRows, numOst;
+let controlNumMax, controlWidth;
+do {
+    numMax = +prompt('Введите количество изображений', numMax);
+    if (numMax > 0) {
+        controlNumMax = true;
+    } else {
+        controlNumMax = false;
+        alert ('Введенное значение не удоветворяет условию задачи!');
+    }
+} while (controlNumMax != true);
+do {
+    width = +prompt('Введите ширину изображения', width);
+    if ((width > 0) && (width <= maxWidth)) {
+        controlWidth = true;
+    } else {
+        controlWidth = false;
+        alert ('Введенное значение не удоветворяет условию задачи!');
+    };
+} while (controlWidth != true);
 
 // вариант 1
-kolImageRows = Math.trunc( maxWidth / (width + (margin * 2)));
-kolRows = Math.trunc( kolMax / kolImageRows);
-kolOst = kolMax - (kolRows * kolImageRows);
+numImageRows = Math.trunc( maxWidth / (width + (margin * 2)));
+numRows = Math.trunc( numMax / numImageRows);
+numOst = (numMax - (numRows * numImageRows)) || numImageRows;
 
-alert(`Вариант 1 изображений в ряду - ${kolImageRows} , полных рядов - ${kolRows}, изображений в последнем ряду ${kolOst}`);
+alert(`Вариант 1 изображений в ряду - ${numImageRows} , полных рядов - ${numRows}, изображений в последнем ряду ${numOst}`);
 
 // вариант 2
-kolRows = ((kolMax * (width + margin * 2)) - (kolMax * (width + margin * 2)) % maxWidth) / maxWidth; 
-kolImageRows = (maxWidth  - (maxWidth %  (width + margin * 2))) /  (width + margin * 2);
-kolOst = kolMax - (kolRows * kolImageRows);
-alert(`Вариант 2 изображений в ряду - ${kolImageRows} , полных рядов - ${kolRows}, изображений в последнем ряду ${kolOst}`);
+numRows = ((numMax * (width + margin * 2)) - (numMax * (width + margin * 2)) % maxWidth) / maxWidth; 
+numImageRows = (maxWidth  - (maxWidth %  (width + margin * 2))) /  (width + margin * 2);
+if (numMax - (numRows * numImageRows) == 0) {
+    numOst = numImageRows;
+} else {
+    numOst = numMax - (numRows * numImageRows);
+};
+alert(`Вариант 2 изображений в ряду - ${numImageRows} , полных рядов - ${numRows}, изображений в последнем ряду ${numOst}`);
 
