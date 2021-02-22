1b53d6a3fb8226d1749df975b73cab6bd47f54cc
Episod86 <episod_kufar@mail.ru>
2021-02-22 21:51:30 +0300

    task6

--- js/lesson5/main.js
+++ js/lesson5/main.js
@@ -1,60 +1,57 @@
 'use strict';
 
 // Задание1.
-// let mesage, i = 1;
-// while (i <=9) {
-//     if (i === 1) {
-//         mesage = `        Квадрат   ${i} =  ${i ** 2}`;
-//     } else {
-//         mesage = `${mesage}
-//         Квадрат   ${i} =  ${i ** 2}`;
-//     };    
-//     i ++;
-// };
-// alert(mesage);
+let mesage, i = 1;
+while (i <=9) {
+    (i === 1) ? mesage = `        Квадрат   ${i} =  ${i ** 2}` : 
+        mesage = `${mesage}
+        Квадрат   ${i} =  ${i ** 2}`;   
+    i ++;
+};
+alert(mesage);
 
 
 // Задание2.
-// const number = +prompt('Введите целое положительное число');
-// let i = 1;
-// let interValue = 1;
-// while (i <= number) {
-//     if ((interValue % 2 == 0) && (interValue % 5 != 0)) {
-//         alert (interValue);
-//         i++;
-//         interValue++;
-//     } else {
-//         interValue++;
-//     };
-// };
+const number = +prompt('Введите целое положительное число');
+for (let i = 2; i <= number * 2; i = i + 2) {
+    if (i % 5 != 0) {
+        alert ( i )
+    };
+};
 
 // Задание3.
-// let num, numberSum = 0;
-// while (num = +prompt('Введите число')) {
-//     numberSum = numberSum + num;
-// }
-// alert (numberSum);
+let num, numberSum = 0;
+while (num = +prompt('Введите число')) {
+    numberSum = numberSum + num;
+}
+alert (numberSum);
 
 
 // Задание4.
-// const n = +prompt('Введите целое положительное число');
-// let sumN = 0;
-// for (let i = 0; i <= n; i++) {
-//     sumN = sumN + i;
-// }
-// alert (sumN);
+const n = +prompt('Введите целое положительное число');
+let sumN = 0;
+for (let i = 0; i <= n; i++) {
+    sumN = sumN + i;
+}
+alert (sumN);
 
 // Задание 5
-// const x = +prompt('Введите целое положительное число');
-// for (let i = 1; i <= x; i++) {
-//     if ( x % i == 0) {
-//         alert (i);
-//     };    
-// }
-
+const x = +prompt('Введите целое положительное число');
+for (let i = 1; i <= x; i++) {
+    if ( x % i == 0) {
+        alert (i);
+    };    
+};
 
 // Задание 6 
-// const y = +prompt('Введите целое положительное число');
-// let sumDigit;
+const y = +prompt('Введите целое положительное число');
+let sumDigit;
+let one, two;
+for (let i = 10; (i < 100); i++) {   
+    if ((Math.trunc(i / 10) + (i % 10)) === y) {
+        alert( i )
+    };
+};
 
-// Вывести на экран все двузначные числа сумма цифр которых делится на  введенное число n.
+// Как перевести число в строку, потом выделить из строки нужный символ и перевести его обратно в число?
+// пробовала substr, но что то пошло не так)
