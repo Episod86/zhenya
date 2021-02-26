44410f4739a228682e510e876210d0cc82d7b565
Episod86 <episod_kufar@mail.ru>
2021-02-26 20:04:44 +0300

    Испарвления

--- figma_proekt/js/maps.js
+++ figma_proekt/js/maps.js
@@ -1,13 +1,38 @@
-let mymap = L.map('mapid').setView([59.944758, 30.339825], 10);
-    L.tileLayer('https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=01F0HVQLQFmbwudW5N0f', {
-    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-    maxZoom: 18
-    // id: 'mapbox/streets-v11',
-    // tileSize: 512,
-    // zoomOffset: -1,
-    // accessToken: 'your.mapbox.access.token'    
-}).addTo(mymap);
-
-    let marker = L.marker([59.944758, 30.339825]).addTo(mymap);
-
-    marker.bindPopup('<b>HouseVOP</b><br> Жилой комплекс').openPopup();
+const cardMaps = L.tileLayer(
+    "https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=01F0HVQLQFmbwudW5N0f",
+    {
+      attribution:
+        '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+      maxZoom: 18,
+    }
+);
+const sputnikMaps = L.tileLayer(
+    "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+    {
+      attribution: "&copy; ArcGIS",
+      maxZoom: 18,
+    }
+);
+let mymap = L.map("mapid", {
+    center: [59.944758, 30.339825],
+    zoom: 10,
+    layers: [sputnikMaps, cardMaps],
+});
+  
+const baseMaps = {
+    Спутник: sputnikMaps,
+    Карта: cardMaps,
+};
+  
+L.control.layers(baseMaps).addTo(mymap);
+  
+L.control
+    .scale({
+      imperial: false,
+    })
+    .addTo(mymap);
+  
+let marker = L.marker([59.944758, 30.339825]).addTo(mymap);
+  
+marker.bindPopup("<b>HouseVOP</b><br> Жилой комплекс").openPopup();
+  
