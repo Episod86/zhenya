ce447e2710b52f7eb078b3519321888eef824ec5
episod86 <episod_kufar@mail.ru>
2021-04-05 08:31:32 +0300

    task1

--- js/lesson14/index2.js
+++ js/lesson14/index2.js
@@ -8,7 +8,7 @@ blueEl.style.backgroundColor = 'blue';
 
 const colorEl = document.querySelectorAll('.yellow');
 for (let i = 0; i < colorEl.length; i++) {
-    if (i === 0 || i ===colorEl.length - 1) {
+    if (i === 0 || i === colorEl.length - 1) {
         colorEl[i].style.backgroundColor = 'yellow'
     } else {
         colorEl[i].style.backgroundColor = 'green'
