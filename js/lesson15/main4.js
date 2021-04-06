cc81de60b5747ae0666861e6579921122ae61a92
episod86 <episod_kufar@mail.ru>
2021-04-06 08:24:12 +0300

    task4

--- js/lesson15/main4.js
+++ js/lesson15/main4.js
@@ -3,7 +3,11 @@
 const elementTr = document.querySelectorAll('tr');
 console.log(elementTr);
 
+console.log(elementTr[0].innerHTML)
 
+if (elementTr[3].innerHTML === elementTr[4].innerHTML) {
+    console.log ('true')
+}
 // В таблице допущена ошибка. Строка Петрова А.И…. вставлена в таблицу дважды.( 3 и 4 строка).  
 // Исправьте эту ошибку при помощи метода   node.remove()
 // Вставьте в начале и в конце раздела  tbody  HTML код, чтобы таблица приобрела вид в соответствии с образцом на рисунке
