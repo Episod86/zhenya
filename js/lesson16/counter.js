3afee0957a822c50990d0e739e39b28ddf4d226c
Episod86 <episod_kufar@mail.ru>
2021-04-16 07:25:11 +0300

    from

--- js/lesson16/counter.js
+++ js/lesson16/counter.js
@@ -1,12 +1,47 @@
-let from;
-do {
-    from = +prompt('Введите начало отсчета', 0);
-} while (isNaN(from) || from <= 0);
+document.body.insertAdjacentHTML('afterbegin', '<div class = "counter"></div>');
+const counter = document.querySelector('.counter');
 
-let to;
-do {
-    to = +prompt('Введите конечную цифру', 0);
-} while (isNaN(to) || to < 0 || to > from);
+// const inputFrom = () => {
+//     let a
+//     do {
+//         a = prompt('Введите начало отсчета', 0);
+//     } while (isNaN(+a) || +a <= 0 || +a === null);
+//     return +a;
+// }   
 
-document.body.insertAdjacentHTML('afterbegin', '<div></div>');
+const inputTo = () => {
+    let b
+    do {
+        b = prompt('Введите конечную цифру', 0);
+    } while (isNaN(b) || +b < 0 || +b > +a || +b === null);
+    return +b;
+};
+
+// inputFrom()
+
+printNumber();
+
+function printNumber() {
+    const from = () => {
+        let a
+        do {
+            a = prompt('Введите начало отсчета', 0);
+        } while (isNaN(+a) || +a <= 0 || +a === null);
+        return +a;
+    }   ;
+    from()
+    console.log(from())
+    to = inputTo;
+
+    let timerId = setInterval(() => {
+        counter.innerHTML = `Отсчет пошел: ${from}`;
+        if (from == to) {
+            clearInterval(timerId);
+        };
+        from--;
+    }, 1000);
+}
+
+
+
 
