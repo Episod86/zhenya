5a8d01fdcbad530da461042a7cff7ea0d9044c25
Episod86 <episod_kufar@mail.ru>
2021-04-29 07:15:01 +0300

    mess

--- js/lesson18/index.js
+++ js/lesson18/index.js
@@ -4,38 +4,56 @@ const counter = document.querySelector('.counter');
 const startButton = document.querySelector('.button');
 const inputNumber = document.querySelectorAll('.input-point')
 const inputError = document.querySelectorAll('.error');
-const inputMess = document.querySelectorAll('input');
+// const inputMess = document.querySelectorAll('input');
+
+const textError = [
+    {
+        title: 'начальное',
+        error: 'больше 0 и больше конечного значения',
+    },
+    {
+        title: 'конечное',
+        error: 'меньше начального, но не меньше 0',
+    },
+];
+
+const textMess = [
+    {
+        title: 'Запустить',
+    },
+    {
+        title: 'Остановить',
+    },
+];
 
 let startNumber;
 let endNumber;
 
-const checkCorrect = (i) => {
-    let n = (i === 0) ? 1 : 0;
+const checkCorrect = (i, n) => {
     if (!inputNumber[n].value) {
-        inputMess[n].innerHTML = `Введите  ${(n === 0) ?  'начальное' : 'конечное'} значение`;
+        inputError[n].innerHTML = `Введите  ${textError[i].title} значение`;
     } else {
-        a = Number(inputNumber[0].value);
-        b = Number(inputNumber[1].value);
-        if (a === 0 || a <= b) { 
-            inputMess[i].innerHTML = `Значение должно быть  ${(n === 1) ?  ' больше 0 и больше конечного значения' : 
-            'меньше начального, но не меньше 0'}`;
+        startNumber = Number(inputNumber[0].value);
+        endNumber = Number(inputNumber[1].value);
+        if (startNumber <= 0 || startNumber <= endNumber) {
+            inputError[i].innerHTML = `Значение должно быть  ${textError[i].error}`;
         } else {
-            for(i = 0; i < inputNumber.length; i++) {
-                inputNumber[i].setAttribute('disabled','disabled');
-                inputMess[i].innerHTML = '';
+            for (i = 0; i < inputNumber.length; i++) {
+                inputNumber[i].setAttribute('disabled', 'disabled');
+                inputError[i].innerHTML = '';
             };
             startButton.removeAttribute('disabled');
         };
-    };    
+    };
 };
 
 const printNumber = (from, to) => {
     let timerId = setInterval(() => {
         if (from === to || !(counter.classList.contains("counter-active"))) {
             if (from === to) {
-                startButton.setAttribute('disabled','disabled');
+                startButton.setAttribute('disabled', 'disabled');
                 startButton.value = 'Пуск';
-                for(let i = 0; i < inputNumber.length; i++) {
+                for (let i = 0; i < inputNumber.length; i++) {
                     inputNumber[i].removeAttribute('disabled');
                     inputNumber[i].value = '';
                 };
@@ -45,14 +63,25 @@ const printNumber = (from, to) => {
         } else {
             counter.innerHTML = `Отсчет пошел: ${from}`;
             from--;
-            a = from;
+            startNumber = from;
         }
     }, 1000);
 };
 
-for(let i = 0; i < inputNumber.length; i++) {
-    inputNumber[i].addEventListener('change', () => checkCorrect(i));
+const showMess = () => {
+    startButton.insertAdjacentHTML('afterend', `<div class = "message">УРА</div>`);
+    let coords = startButton.getBoundingClientRect();
+    console.log(coords)
+}
+
+for (let i = 0, n = 1; i < inputNumber.length; i++, n--) {
+    inputNumber[i].addEventListener('change', () => checkCorrect(i, n));
 };
+
+// for (let i = 0; i < inputMess.length; i++) {
+//     inputMess[i].addEventListener('pointerover', () => showMess(i));
+//     // inputMess[i].addEventListener('pointerout', () => clearMess(i));
+// };
 
 startButton.addEventListener('click', () => {
     if (startButton.value === 'Пуск') {
@@ -62,8 +91,12 @@ startButton.addEventListener('click', () => {
         startButton.value = 'Пуск';
         counter.classList.remove("counter-active");
     };
-    printNumber(a, b);
+    printNumber(startNumber, endNumber);
 });
+
+startButton.addEventListener('pointerover', () => showMess());
+// startButton.addEventListener('pointerout', () => clearMess());
+
 
 
 
